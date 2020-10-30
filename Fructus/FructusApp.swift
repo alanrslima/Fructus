1e1a69fed6c9008e774f62cffb78b93ec31bc636
alanrslima <alanronison.lima@gmail.com>
2020-10-30 01:34:24 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Alan Lima on 30/10/20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
